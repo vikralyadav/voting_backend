113ece01ab7d2702a48246cfdd20633c17877dbb
vikralyadav <yadavvikral2003@gmail.com>
2024-12-12 11:55:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const db = require('./db');
+require('dotenv').config();
+
+
+
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
+
+
+const userRoutes = require('./routes/userRoutes'); 
+const candidateRoutes = require('./routes/candidateRoutes'); 
+
+
+app.use('/user', userRoutes);
+console.log("gndfkjghfdkjgkj");
+
+app.use('/candidate', candidateRoutes);
+
+
+
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
+
+
